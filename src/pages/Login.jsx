efa385bacd713aab75fad8d56763721dd4c44046
unknown <abdulmukti40390@gmail.comgit config --global.user.mukti>
2024-07-02 18:22:08 +0700

    change redirectUrl

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ function Login() {
   const token = useStore((state) => state.token);
   const navigate = useNavigate();
   const handleClick = () => {
-    const redirectUrl = "http://localhost:5173/";
+    const redirectUrl = "https://spotify-woad-psi.vercel.app";
     const clientId = `2519e3e45f514f058835ac9e90f2cf92`;
     const apiUrl = "https://accounts.spotify.com/authorize";
     window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scopes=${scopes.join(
